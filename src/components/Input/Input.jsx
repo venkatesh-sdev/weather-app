1ba7f246fa8222f428ecad9fd649f6b10e7f11cd
Venkatesh S <venkatesh.dev.main@gmail.com>
2023-02-16 12:37:32 +0530

    test

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -9,7 +9,7 @@ const Input = ({ inputValue, inputRef, setInputValue, prevRef, nextRef, spanData
             maxLength='1'
             pattern='[0-9]'
             onKeyDown={e => {
-                alert(e.which)
+                alert(e.keyCode)
                 if (e.key === 'Backspace') {
                     setInputValue('')
                     prevRef.current.focus();
