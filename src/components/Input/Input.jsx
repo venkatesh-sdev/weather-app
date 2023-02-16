4f71fa5cae31286bdb55481ab6bf26d14c16a68a
Venkatesh S <venkatesh.dev.main@gmail.com>
2023-02-16 12:46:46 +0530

    input problem Cleared

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -8,20 +8,18 @@ const Input = ({ inputValue, inputRef, setInputValue, prevRef, nextRef, spanData
             type='text'
             maxLength='1'
             pattern='[0-9]'
-            onKeyDown={e => {
-                alert(e.keyCode)
-                if (e.key === 'Backspace') {
-                    setInputValue('')
-                    prevRef.current.focus();
-                } else {
-                    if(e.key=='0'||e.key=='1'||e.key=='2'||e.key=='3'||e.key=='1'||e.key=='4'||e.key=='5'||e.key=='6'||e.key=='7'||e.key=='8'||e.key=='9'){
-                        setInputValue(e.key)
-                        nextRef.current.focus();
-                    }else{
-                        prevRef.current.focus();
-                    }
-
+            onChange={(e) => {
+                if (e.target.value == '0' || e.target.value == '1' || e.target.value == '2' || e.target.value == '3' || e.target.value == '4' || e.target.value == '5' || e.target.value == '6' || e.target.value == '7' || e.target.value == '8' || e.target.value == '9') {
+                    setInputValue(e.target.value)
+                    nextRef.current.focus();
+                    return
                 }
+            }}
+            onKeyDown={e => {
+                if (e.key === 'Backspace') {
+                setInputValue('')
+                prevRef.current.focus();
+                } 
             }}
             onFocus={() => {
                 spanRef.current.style.transform = `translateX(${spanData}px)`
